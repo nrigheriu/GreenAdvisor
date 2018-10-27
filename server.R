3da60cac6705df3e9a3704df1db3063981766e7c
bormara <b.marazuela@alumnos.upm.es>
2018-10-27 11:32:33 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,17 +1,23 @@
-
-
 shinyServer(function(input, output, session){
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-  
+  saveData<-function(data){
+    #data <- as.data.frame(t(data))
+    df$evaluations[df$basicData.name == data$name]<-df$evaluations[df$basicData.name == data$name] +1
+    df$grade[df$basicData.name == data$name]<-df$grade[df$basicData.name == data$name]+data$nota
+    
+  }
+  formData <- reactive({
+    data <- sapply(fields)
+    data
+  })
+  observeEvent( input$save,{
+    saveData(formData)
+  })
   output$mymap <- renderLeaflet({
     leaflet() %>%
       addProviderTiles(providers$Stamen.TonerLite,
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addMarkers(lng = restaurants$geoData.longitude, lat = restaurants$geoData.latitude)
+      addMarkers(lng = df$geoData.longitude, lat = df$geoData.latitude)
   })
 }
-  )
+)
