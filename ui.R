90e0818cb4837f7365397ced861b819daa4c9b85
bormara <b.marazuela@alumnos.upm.es>
2018-10-27 11:32:12 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,5 +1,13 @@
-shinyUI(fluidPage(id = "main",
-                  leafletOutput("mymap")
-                  
-                  
-))
+ui <- fluidPage(
+  titlePanel("Valorar"),
+  sidebarPanel(
+    selectInput("name", "restaurante", choices = restaurantlist),
+    selectInput("nota", "nota", c("1","2","3","4","5")),
+    actionButton("save", "Add")
+  ),
+  mainPanel(
+    
+    mainPanel(leafletOutput("mymap",height = 750, width=750))
+    
+  )
+)
